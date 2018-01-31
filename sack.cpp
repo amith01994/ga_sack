388dd7eeedf6671a231917728d7a9022b5f34f5e
Ishara Upamal <amith01994@gmail.com>
2018-01-31 12:58:03 +0530

    second commit

--- sack.cpp
+++ sack.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+using namespace std;
+int no_of_items = 0;
+int knapsack_cap = 88;
+int current_capacity = 0;
+
+int main(){
+  cout << "Number of items:";
+  cin >> no_of_items;
+  //cout << "No of items--->" << no_of_items << endl;
+
+  cout << "Input space seperated weight and value pairs:" << endl;
+
+
+
+
+}
