35c5372505ff87313c233434b8007ca80bea7f37
Ishara Upamal <amith01994@gmail.com>
2018-01-31 13:19:54 +0530

    third

--- sack.cpp
+++ sack.cpp
@@ -8,8 +8,20 @@ int main(){
   cout << "Number of items:";
   cin >> no_of_items;
   //cout << "No of items--->" << no_of_items << endl;
+  int weights_arr[no_of_items];
+  int value_arr[no_of_items];
+
 
   cout << "Input space seperated weight and value pairs:" << endl;
+  for(int i = 0; i < no_of_items;i++){
+    cin >> weights_arr[i];
+    cin >> value_arr[i];
+
+  }
+  cout << "Weight:"<<weights_arr[0] << endl;
+  cout << "Value:"<<value_arr[0] << endl;
+
+
 
 
 
